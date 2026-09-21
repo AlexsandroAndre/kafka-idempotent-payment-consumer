530227e451d844da6ad81ab3192528f3d76309d8
Alexsandro André <contatoalexsandroandre@gmail.com>
2026-09-20 22:57:11 -0300

    correcao dos testes

--- src/test/java/com/alexsandroandre/kafka_idempotent_payment_consumer/support/PostgresIntegrationTestBase.java
+++ src/test/java/com/alexsandroandre/kafka_idempotent_payment_consumer/support/PostgresIntegrationTestBase.java
@@ -3,19 +3,19 @@ package com.alexsandroandre.kafka_idempotent_payment_consumer.support;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 public abstract class PostgresIntegrationTestBase {
 
     private static final String POSTGRES_16_ALPINE = "postgres:16-alpine";
 
-    @Container
     static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>(POSTGRES_16_ALPINE)
             .withDatabaseName("payment_db")
             .withUsername("payment_user")
             .withPassword("payment_pass");
+
+    static {
+        POSTGRES.start();
+    }
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
